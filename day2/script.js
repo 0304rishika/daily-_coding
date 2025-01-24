41ae1b954e67219c485f9467e8f8833dbf43b07b
0304rishika <reddyrishika978@gmail.com>
2025-01-24 12:17:05 +0530

    first code

---
+++ day2/script.js
@@ -0,0 +1,55 @@
+//1. How many data types are there in JavaScript?
+//ans)JavaScript has 8 data types
+
+//2. What kind of numbers are represented by the number type in JavaScript?
+//ans)JavaScript represents by integers
+//    Integers: 10, -3
+
+//3. What is the limitation of the number data type?
+
+//4. How is a BigInt instantiated (created) in JavaScript?
+//ans)let b = 123456789012345678901234567890n;
+
+//5. Which are the three types of quotes that can be used to create a string representation of a value in JavaScript?
+//ans) 1. Double quotes: "Hello"
+//     2. Single quotes: 'World'
+
+//6. What is the difference between double, single, and backtick quotes?
+//ans)Double quotes (")
+//    Single quotes (')
+
+//7. What are the two possible values of the Boolean data type?
+//ans)true
+//   false
+
+//8. What does the null data type represent in JavaScript?
+//ans)let a = null;
+
+//9. What does the undefined data type represent in JavaScript?
+//ans) let a;
+//    console.log(a); 
+
+//10. What is the difference between typeof x and typeof(x) with parentheses?
+//ans)There is no difference.
+
+//11. What does the typeof operator return?
+
+//12. What are the primitive data types in JavaScript?
+//ans)1. Number 2. BigInt 3. String 4. Boolean 5. Null 6. Undefined 7. Symbol
+
+//13. What is the difference between null and undefined in JavaScript?
+//ans)let a = null;         
+    //let b;                
+
+//14. What is NaN in JavaScript?
+//ans)
+
+//15. What is a BigInt in JavaScript?
+//ans)BigInt is numeric data type that can represent integers  for the number type (12345n).
+
+
+//16. What is the difference between a primitive data type and an object data type in JavaScript?
+
+//ans)Primitive data types: stored directly by value.
+     //Object data types:stored by reference
+     //  Examples: Array, Function, Object
