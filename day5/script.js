9e2904d2ec007f8a74f7660a1c37a389a4175a5e
0304rishika <reddyrishika978@gmail.com>
2025-01-29 20:10:49 +0530

    first code

---
+++ day5/script.js
@@ -0,0 +1,227 @@
+// 1.What will be the output of the following code?
+
+// let x = 10;
+// if (x > 5) {
+//     console.log("Greater than 5");
+// }
+//ans)output:Greater than 5
+ 
+// 2.Write a program to check if a number is positive. If it is, print "Positive number."
+//ans)let y = 7; 
+//    if (y > 0) {
+//    console.log("Positive number");
+//    }
+
+// 3.Write a program to check if a person is eligible to vote. A person is eligible if their age is 18 or above. If eligible, print "Eligible to vote."
+//ans) let z = 18;
+//     if (z >= 18) {
+//     console.log("eligible to vote");
+//     } 
+
+
+// 4.Write a program to check if a number is divisible by 2. If it is, print "Divisible by 2."
+//ans)let k = 8; 
+//    if (k%2) {
+//     console.log("Divisible by 2");
+//    }
+
+// 5.What will be the output of the following code?
+
+// let score = 75;
+// if (score >= 50) {
+//     console.log("Passed");
+// }
+//ans)output:passed
+
+// 6.Write a program to check if the time is greater than or equal to 12 PM. If true, print "Afternoon."
+//ans) let time=1pm;
+//     if (time>= 12) {
+//     console.log("Afternoon");
+//   } 
+
+// 7.Write a program that checks if a number is greater than or equal to 100. If it is, print "Large number."
+//ans) let s = 190; 
+//    if (s >= 100) {
+//    console.log("Large number");
+//    } 
+
+// 8.Write a program to check if a person’s age is less than 18. If true, print "Minor."
+//ans)let m = 16; 
+//    if (m < 18) { 
+//    console.log("Minor");
+//    } 
+
+// 9.Given a variable temperature = 28, write an if statement to check if the temperature is above 25 degrlet temperature = 28;
+//ans)let t =28;
+//    if (t > 25) {
+//    console.log("It's hot outside");
+//    } 
+
+// 10. Given the following code, what will the output be?
+
+// let number = 0;
+// if (number === 0) {
+//     console.log("Zero");
+// }
+//ans)output:zero
+
+// 11.Write a program to check if a number is positive or negative. If it’s positive, print "Positive number"; otherwise, print "Negative number."
+//ans)let n = -5; 
+//    if (n > 0) {
+//   console.log("Positive number");
+//   } else (n < 0) {
+//  console.log("Negative number");
+//   } 
+
+// 12.What will be the output of the following code?
+
+// let x = 10;
+// if (x < 5) {
+//     console.log("Less than 5");
+// } else {
+//     console.log("Greater than or equal to 5");
+// }
+//ans) output:Greater than or equal to 5
+
+
+// 14.Write a program that checks if a number is even or odd. If it is even, print "Even number"; otherwise, print "Odd number."
+//ans)let p =8; 
+//    if (p%2) {
+//    console.log("even number");
+//    } else {
+//    console.log("odd nummber");
+//    }
+
+// 15.Write a program to check if a person is eligible to vote. If the person is 18 or older, print "Eligible to vote"; otherwise, print "Not eligible to vote."
+//ans)let a = 19;
+//   if (age >= 18) {
+//   console.log("eligible to vote");
+//   } else {
+//   console.log("Not eligible to vote");
+//   }
+
+// 16.What will be the output of the following code?
+
+// let score = 85;
+// if (score >= 90) {
+//     console.log("Grade A");
+// } else {
+//     console.log("Grade B");
+// }
+//ans)output:Grade B
+
+
+// 17.Write a program to check if a number is greater than or equal to 100. If it is, print "Large number"; otherwise, print "Small number."
+//ans)let p = 80; 
+//    if (p >= 100) {
+//    console.log("Large number");
+//    } else {
+//    console.log("Small number");
+//    }
+
+// 18.Write a program to check if the temperature is greater than or equal to 30 degrees. If it is, print "Hot day"; otherwise, print "Cool day."
+// ans)let t = 45; 
+//     if (t>= 30) {
+//     console.log("Hot day");
+//     } else {
+//     console.log("Cool day");
+//    }
+
+// 19.Write a program to check if a person is a teenager (age between 13 and 19). If the person is a teenager, print "Teenager"; otherwise, print "Not a teenager."
+// What will be the output of the following code?
+
+// 20.Write a program to check if a number is positive and even. If both conditions are true, print "Positive Even number"; otherwise, print "Not a Positive Even number."
+// ans)let n= 24; 
+//     if (n > 0 ) {
+//     console.log("Positive Even number");
+//     } else {
+//     console.log("Not a Positive Even number");
+//     }
+
+// 21.What will be the output of the following code?
+
+// let age = 20;
+// if (age >= 18) {
+//     if (age >= 21) {
+//         console.log("Adult and eligible to drink");
+//     } else {
+//         console.log("Adult but not eligible to drink");
+//     }
+// } else {
+//     console.log("Not an adult");
+// }
+
+// 22.Write a program to check if a number is greater than 0 and divisible by 5. If both conditions are true, print "Positive and divisible by 5"; otherwise, print "Either not positive or not divisible by 5."
+
+// 23.Write a program to check if a student’s marks are above 50 and if the marks are greater than 75, print "Excellent", else print "Good". If the marks are 50 or below, print "Try harder next time."
+
+// 24.What will be the output of the following code?
+
+// let temperature = 35;
+// if (temperature > 30) {
+//     if (temperature > 40) {
+//         console.log("It's extremely hot!");
+//     } else {
+//         console.log("It's hot outside!");
+//     }
+// } else {
+//     console.log("It's cool outside!");
+// } 
+
+// 25.Write a program to check if a person is eligible for senior citizen benefits (age 60 or above). If the person is eligible, check if the person is a resident of the country. If both conditions are true, print "Eligible for benefits"; otherwise, print "Not eligible."
+
+
+// 26.What will be the output of the following code?
+// let score = 80;
+// if (score >= 90) {
+//     console.log("Grade A");
+// } else {
+//     if (score >= 70) {
+//         console.log("Grade B");
+//     } else {
+//         console.log("Grade C");
+//     }
+// }
+//ans)output:Grade B
+
+// 27.Write a program to check if a person is eligible for a discount (age between 13 and 25). If eligible, check if the person has a student ID. If both conditions are true, print "Eligible for discount"; otherwise, print "Not eligible for discount."
+
+// 28.Write a program to check if a number is greater than or equal to 100 and if it is also divisible by 10. If both conditions are true, print "Large and divisible by 10"; otherwise, print "Either not large or not divisible by 10."
+//ans)let n = 120;
+//     if (number >= 100) {
+//     console.log("Large and divisible by 10");
+//     } else {
+//     console.log("Either not large or not divisible by 10");
+//     }
+
+// 29.What will be the output of the following code?
+
+// let time = 14;
+// if (time >= 12) {
+//     if (time < 18) {
+//         console.log("Good Afternoon");
+//     } else {
+//         console.log("Good Evening");
+//     }
+// } else {
+//     console.log("Good Morning");
+// }
+//ans)
+
+
+// let number = 0;
+// if (number > 0) {
+//     console.log("Positive number");
+// } else {
+//     console.log("Non-positive number");
+// } 
+//ans) output:Positive number 
+
+// 30.Write a program to check if a number is greater than 50. If it is, print "Above 50"; otherwise, print "50 or below."
+// ans)let number = 45; 
+//    if (number > 50) {
+//    console.log("Above 50.");
+//    } else {
+//   console.log("50 or below.");
+//    }
+
